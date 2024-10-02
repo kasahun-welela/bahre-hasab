c3079df750a1a0dc6df1519ed1ca22eea669ad04
kasahun-welela <kasahunwelela1@gmail.com>
2024-10-02 23:15:00 +0300

    calculate for the fasting

--- src/components/CalculateHolidays.tsx
+++ src/components/CalculateHolidays.tsx
@@ -44,6 +44,59 @@ function CalculateHolidays() {
     }
   };
 
+  const calculateDayOfWeek = (firstDay: any, day: any) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const firstDayIndex = days.indexOf(firstDay);
+    const dayIndex = (firstDayIndex + (day - 1)) % 7;
+    return days[dayIndex];
+  };
+
+  const monthsOfYear = (month: any) => {
+    const months = [
+      "Meskerem",
+      "Tikmt",
+      "Hidar",
+      "Tesas",
+      "Tirr",
+      "Yekatit",
+      "Megabit",
+      "Miyaziya",
+      "Ginbot",
+      "Sene",
+      "Hamle",
+      "Nehase",
+    ];
+    const monthIndex = months.indexOf(month);
+    const dayIndex = monthIndex + 1;
+    return months[dayIndex];
+  };
+
+  const returnEletTewsak = (n: string) => {
+    switch (n) {
+      case "Monday":
+        return 6;
+      case "Tuesday":
+        return 5;
+      case "Wednesday":
+        return 4;
+      case "Thursday":
+        return 3;
+      case "Friday":
+        return 2;
+      case "Saturday":
+        return 8;
+      default:
+        return 7;
+    }
+  };
   const calculate = () => {
     const ameteKunene = 5500;
     const ameteMhret = 2017;
@@ -57,16 +110,101 @@ function CalculateHolidays() {
     const abekte = (wenber * 11) % 30;
     const metk = (wenber * 19) % 30;
     let bealeMetk = "";
-    if (metk > 14) bealeMetk = "Meskrem";
+    if (metk > 14) bealeMetk = "Meskerem";
     else bealeMetk = "Tikimt";
 
-    console.log("wongelawi", wongelawi);
-    console.log("metene rabit", meteneRabit);
-    console.log("enkutatash", eleteEnkutatash);
-    console.log("medeb", medeb);
-    console.log("abekte", abekte);
-    console.log("metk", metk);
-    console.log(" bealemetk", bealeMetk, " ", metk);
+    // help us to get the day of bealeMetk
+    const firstDay = eleteEnkutatash;
+    const eleteMetk = calculateDayOfWeek(firstDay, metk);
+    const tewsakElet = returnEletTewsak(eleteMetk);
+    //calculating feast of Nineveh / Nenewe
+    const neneweTsomMegbiyaWer = bealeMetk == "Meskerem" ? "Tirr" : "Yekatit";
+
+    const mebajaHamer = (tewsakElet + metk) % 30;
+    // const neneweTsom = tewsakElet + metk > 30 ? monthsOfYear("Tirr") : "Tirr";
+    const nenewe = calculateFastingAndHoliday(
+      tewsakElet,
+      metk,
+      neneweTsomMegbiyaWer
+    );
+    const abiyTsom = calculateFastingAndHoliday(
+      14,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const debreZeit = calculateFastingAndHoliday(
+      11,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const Hosanna = calculateFastingAndHoliday(
+      2,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const siklet = calculateFastingAndHoliday(
+      7,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const tinsae = calculateFastingAndHoliday(
+      9,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const rkbeKahnat = calculateFastingAndHoliday(
+      3,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const erget = calculateFastingAndHoliday(
+      18,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const peraklitos = calculateFastingAndHoliday(
+      28,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const tsomeHawariyat = calculateFastingAndHoliday(
+      29,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+    const tsomeDhnet = calculateFastingAndHoliday(
+      1,
+      mebajaHamer,
+      neneweTsomMegbiyaWer
+    );
+
+    // console.log("wongelawi", wongelawi);
+    // console.log("metene rabit", meteneRabit);
+    // console.log("enkutatash", eleteEnkutatash);
+    // console.log("medeb", medeb);
+    // console.log("abekte", abekte);
+    // console.log("metk", metk);
+    // console.log(" bealemetk", bealeMetk, " ", metk);
+    // console.log("abekte day", eleteMetk);
+    // // console.log("nenewe", neneweTsomMegbiyaWer, mebajaHamer);
+    // // console.log("nenewe megbiya", neneweTsomMegbiyaWer, mebajaHamer);
+    // console.log("nenewe", nenewe);
+    console.log("nenewe", nenewe);
+    console.log("abiy tsom", abiyTsom);
+    console.log("debre zeit", debreZeit);
+    console.log("hossana", Hosanna);
+    console.log("siklet", siklet);
+    console.log("tinsae", tinsae);
+    console.log("rkbe kahnat", rkbeKahnat);
+    console.log("erget", erget);
+    console.log("perakitos", peraklitos);
+    console.log("tsome hawariyat", tsomeHawariyat);
+    console.log("tsome dhnet", tsomeDhnet);
+  };
+  const calculateFastingAndHoliday = (tewsak: any, metk: any, month: any) => {
+    const currentMonth = tewsak + metk > 30 ? monthsOfYear(month) : month;
+    const day = (tewsak + metk) % 30;
+    return `${currentMonth}  ${day}`;
   };
 
   return (
